19d6a86c34be15be95b7e18f0dbf7b89ce2a35fb
chen jiajing <16602132375@163.com>
2021-10-19 00:18:20 +0800

    feat: 计算器输入组

--- lib/main.dart
+++ lib/main.dart
@@ -13,15 +13,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Calculator',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        backgroundColor: Colors.black,
+        // primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+      ),
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
       ),
       home: const MyHomePage(title: '计算器'),
     );
   }
 }
 
-// home
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
@@ -42,7 +45,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Container(
         padding: const EdgeInsets.symmetric(horizontal: 18),
-        decoration: const BoxDecoration(color: Colors.black),
+        // decoration: const BoxDecoration(color: Colors.black12),
         child: Column(
           children: <Widget>[
             Expanded(
@@ -63,7 +66,9 @@ class _MyHomePageState extends State<MyHomePage> {
             const SizedBox(
               height: 20,
             ),
-            _CalculatorKeyboard(),
+            _CalculatorKeyboard(onValueChange: (String str) {
+              print(str);
+            }),
             const SizedBox(
               height: 80,
             ),
@@ -74,47 +79,131 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-// _CalculatorKeyboard
+// 按钮组
 class _CalculatorKeyboard extends StatelessWidget {
+  final ValueChanged<String> onValueChange;
+  final List<Map> _keyboardList = [
+    {
+      'text': 'AC',
+      'textColor': Colors.black,
+      'color': const Color(0xFFA5A5A5),
+      'highlightColor': const Color(0xFFD8D8D8)
+    },
+    {
+      'text': '+/-',
+      'textColor': Colors.black,
+      'color': const Color(0xFFA5A5A5),
+      'highlightColor': const Color(0xFFD8D8D8)
+    },
+    {
+      'text': '%',
+      'textColor': Colors.black,
+      'color': const Color(0xFFA5A5A5),
+      'highlightColor': const Color(0xFFD8D8D8)
+    },
+    {
+      'text': '÷',
+      'color': const Color(0xFFE89E28),
+      'highlightColor': const Color(0xFFEDC68F)
+    },
+    {'text': '7', 'color': const Color(0xFF363636)},
+    {'text': '8', 'color': const Color(0xFF363636)},
+    {'text': '9', 'color': const Color(0xFF363636)},
+    {
+      'text': 'x',
+      'color': const Color(0xFFE89E28),
+      'highlightColor': const Color(0xFFEDC68F),
+    },
+    {'text': '4', 'color': const Color(0xFF363636)},
+    {'text': '5', 'color': const Color(0xFF363636)},
+    {'text': '6', 'color': const Color(0xFF363636)},
+    {
+      'text': '-',
+      'color': const Color(0xFFE89E28),
+      'highlightColor': const Color(0xFFEDC68F)
+    },
+    {'text': '1', 'color': const Color(0xFF363636)},
+    {'text': '2', 'color': const Color(0xFF363636)},
+    {'text': '3', 'color': const Color(0xFF363636)},
+    {
+      'text': '+',
+      'color': const Color(0xFFE89E28),
+      'highlightColor': const Color(0xFFEDC68F)
+    },
+    {'text': '0', 'color': const Color(0xFF363636), 'width': 158.0},
+    {'text': '.', 'color': const Color(0xFF363636)},
+    {
+      'text': '=',
+      'color': const Color(0xFFE89E28),
+      'highlightColor': const Color(0xFFEDC68F),
+    },
+  ];
+
+  _CalculatorKeyboard({Key? key, required this.onValueChange})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
-      height: 500,
-      child: _CalculatorItem(),
+    return Wrap(
+      runSpacing: 18,
+      spacing: 18,
+      children: List.generate(
+        _keyboardList.length,
+        (index) => _CalculatorItem(
+          color: _keyboardList[index]['color'],
+          text: _keyboardList[index]['text'],
+          highlightColor: _keyboardList[index]['highlightColor'],
+          width: _keyboardList[index]['width'],
+          onValueChange: onValueChange,
+        ),
+      ),
     );
   }
 }
 
-// btn
+// 按钮
 class _CalculatorItem extends StatelessWidget {
   final String text;
-  final Color textColor;
+  final Color? textColor;
   final Color color;
-  final Color highlightColor;
-  final double width;
+  final Color? highlightColor;
+  final double? width;
   final ValueChanged<String> onValueChange;
 
   const _CalculatorItem(
       {required this.text,
-      required this.textColor,
       required this.color,
-      required this.highlightColor,
-      required this.width,
-      required this.onValueChange});
+      required this.onValueChange,
+      this.highlightColor,
+      this.textColor,
+      this.width});
 
   @override
   Widget build(BuildContext context) {
     return Ink(
       decoration: BoxDecoration(
         color: color,
-        borderRadius: const BorderRadius.all(
-          Radius.circular(200),
-        ),
+        borderRadius: const BorderRadius.all(Radius.circular(200)),
       ),
       child: InkWell(
+        borderRadius: const BorderRadius.all(Radius.circular(200)),
+        highlightColor: highlightColor ?? color,
         onTap: () {
-          onValueChange('999999');
+          onValueChange(text);
         },
+        child: Container(
+          width: width ?? 70,
+          height: 70,
+          padding: EdgeInsets.only(left: width == null ? 0 : 25),
+          alignment: width == null ? Alignment.center : Alignment.centerLeft,
+          child: Text(
+            text,
+            style: TextStyle(
+              color: textColor ?? Colors.white,
+              fontSize: 24,
+            ),
+          ),
+        ),
       ),
     );
   }
